e2c3cf22b2e6278477acdea99e4b6ab56631e005
eunyoungroh <roheunyoung@outlook.com>
2019-09-19 16:04:55 +0900

    Second Time - scanf
    learned how to use scanf in C.

--- main.c
+++ main.c
@@ -12,6 +12,17 @@ int main(int argc, char *argv[]) {
 	printf("bytes of short : %d\n", sizeof(y));	
 	printf("bytes of float : %d\n", sizeof(z));
 	printf("bytes of double : %d\n", sizeof(w));	
+	
+	int input_int;
+	float input_float;
+	
+	printf("Please type an integer: ");
+	scanf("%d", &input_int);
+	
+	printf("Please type a float: ");
+	scanf("%f", &input_float);
+	
+	printf("Your integer is %d and your float is %f", input_int, input_float);
 		
 	return 0;
 
