87633d77753b4af84ad9d568d62022dd62901cdb
eunyoungroh <roheunyoung@outlook.com>
2019-09-19 15:32:30 +0900

    Second Time
    Print data sizes of some fundamental data types.

--- main.c
+++ main.c
@@ -3,8 +3,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	printf("Hello, World!\n");
+	int x; // integer
+	short y;
+	float z;
+	double w;
 	
+	printf("bytes of int : %d\n", sizeof(x));
+	printf("bytes of short : %d\n", sizeof(y));	
+	printf("bytes of float : %d\n", sizeof(z));
+	printf("bytes of double : %d\n", sizeof(w));	
+		
 	return 0;
 
 }
